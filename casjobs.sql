815cb4bd3e8980e0252a5a0ed071f59c922c56b3
Jeffery M. Thompson <jthompson@svc.edu>
2015-12-31 14:06:28 -0500

    Updating graph functions to vectorize color

--- casjobs.sql
+++ casjobs.sql
@@ -43,3 +43,30 @@ WHERE ((psfmag_g -(@correction_g * extinction_g)) between 16.0 and 23.0) and
   (flags & dbo.fPhotoFlags('SATURATED')) = 0 and
   (flags & dbo.fPhotoFlags('EDGE')) = 0)))
   
+/*
+   Testing a new query to get better efficiency from the database.
+   pushing all direct comparisions up in order.
+   If CasJob times are still slow I will convert all between statements to direct comparisions.
+*/
+
+declare @correction_u real,
+		@correction_g real,
+		@correction_r real,
+		@correction_i real,
+		@correction_z real
+set @correction_u = 4.239 / 5.155
+set @correction_g = 3.303 / 3.793
+set @correction_r = 2.285 / 2.751
+set @correction_i = 1.698 / 2.086
+set @correction_z = 1.263 / 1.479
+ 
+Select objid, ra, dec, l ,b, psfMag_u, psfMag_g, psfMag_r, psfMag_i, psfMag_z, 
+  extinction_u, extinction_g, extinction_r, extinction_i, extinction_z
+  into mydb.MSTSouth_Neg_86_To_Neg_88 from star
+  WHERE
+    (b <= -86.0) and (b > -88.0) and 
+    ((flags & dbo.fPhotoFlags('SATURATED')) = 0) and
+    ((flags & dbo.fPhotoFlags('EDGE')) = 0) and
+    ((psfmag_g -(@correction_g * extinction_g)) between 16.0 and 23.0) and
+    ((psfmag_g -(@correction_g * extinction_g)) - (psfmag_r -(@correction_r * extinction_r)) between 0.1 and 0.3) and
+    ((psfmag_u -(@correction_u * extinction_u)) - (psfmag_g -(@correction_g * extinction_g)) > 0.4)
