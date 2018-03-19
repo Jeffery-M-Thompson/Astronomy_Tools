af56d9e8da85d2dd037ca983cbd8d9ca995be23d
Jeffery M. Thompson <jthompson@svc.edu>
2018-03-19 16:37:37 -0400

    Turned count mechanism into a function called from main to add other options, no option selection mechanism yet.

--- star_query.py
+++ star_query.py
@@ -5,24 +5,34 @@ import numpy as np
 from astropy.table import Table
 from astroquery.sdss import SDSS
 
+def countStars(b_low, b_high, delta, release, output):
+	count = int((b_high-b_low)/delta)+1
+	scans = np.linspace(b_low, b_high, num=count)
+	counts = Table([[],[],[]],names=('b_low', 'b_high', 'stars'))
+
+	for b in np.nditer(scans):
+		bnext = b + delta
+		low = str(b)
+		high = str(bnext)
+		query = 'Select Count(objID) from star where (b>={0}) and (b<{1})'\
+		.format(low, high)
+		if output:
+			print (query)
+		results = SDSS.query_sql(query, data_release=release, timeout = 300)
+		if output:
+			print (results)
+		counts.add_row([b,bnext,results['Column1'][0]])
+	return counts
+
+def getRows(b_low, b_high, l_low, l_high, delta_b, delta_l, release, output):
+
+	return rows
 b_low  = -90.0
 b_high = 90.0
+delta_b  = 1.0
+release = 14
+output = True
 
-delta  = 1.0
-count = int((b_high-b_low)/delta)+1
-scans = np.linspace(b_low, b_high, num=count)
-counts = Table([[],[],[]],names=('b_low', 'b_high', 'stars'))
-
-for b in np.nditer(scans):
-	bnext = b + delta
-	low = str(b)
-	high = str(bnext)
-	query = 'Select Count(objID) from star where (b>={0}) and (b<{1})'.format(low, high)
-	print (query)
-	results = SDSS.query_sql(query, data_release=14, timeout = 300)
-	print (results)
-	counts.add_row([b,bnext,results['Column1'][0]])
-
-file_name = '/media/pi/Research_Data/Star_Counts_SDSS_DR14/DR14_{0}_to_{1}.csv'\
-.format(str(b_low), str(bnext))
-counts.write(file_name, format='ascii.csv', overwrite=True)
+#results = countStars(b_low,b_high, delta_b, release, output)
+#file_name = '../DR{0}{1}to{2}.csv'.format(str(release),str(b_low), str(b_high))
+#results.write(file_name, format='ascii.csv')
