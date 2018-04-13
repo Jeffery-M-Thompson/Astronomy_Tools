397b589626e531c21ba37d62fbe72c50a5884215
Jeffery M. Thompson <jthompson@svc.edu>
2018-04-13 16:30:30 -0400

    MSTO query stub

--- SDSS_star_queries.py
+++ SDSS_star_queries.py
@@ -26,7 +26,44 @@ def query_star(release):
 
 # This generates a hdf table of all MSTO stars given the 
 
-def query_MSTO(release):
+def query_MSTO(release, dered):
+	if dered:
+			queryString = ('Select objId
+	else:
+		
 	
 	return MSTO_Table
+	
+	
+declare @correction_u real,
+		@correction_g real,
+		@correction_r real,
+		@correction_i real,
+		@correction_z real
+set @correction_u = 4.239 / 5.155
+set @correction_g = 3.303 / 3.793
+set @correction_r = 2.285 / 2.751
+set @correction_i = 1.698 / 2.086
+set @correction_z = 1.263 / 1.479
+
+Select ra, dec, l ,b, psfMag_u, psfMag_g, psfMag_r, psfMag_i, psfMag_z, 
+  extinction_u, extinction_g, extinction_r, extinction_i, extinction_z
+  into mydb.MSTSouth_Neg_25_To_Neg_30 from star
+WHERE ((psfmag_g -(@correction_g * extinction_g)) between 16.0 and 23.0) and
+  (b <= -25.0) and (b > -30.0) and
+  (((psfmag_g -(@correction_g * extinction_g) - 
+  		(psfmag_r -(@correction_r * extinction_r)) between 0.1 and 0.3) and
+  (((psfmag_u -(@correction_u * extinction_u) - 
+  (psfmag_g -(@correction_g * extinction_g)) > 0.4) and
+  (flags & dbo.fPhotoFlags('SATURATED')) = 0 and
+  (flags & dbo.fPhotoFlags('EDGE')) = 0
+  
+	
+query = 'Select * from star where (b>={0}) and (b<{1})'.format(b_low, b_high)
+	if output:
+		print (query)
+	results = SDSS.query_sql(query, data_release=release, timeout = 3600)
+	if output:
+		print (results)
+	return results
 	
